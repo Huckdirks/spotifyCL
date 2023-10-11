58a024818f6ac4066c329e156d3bd7af327433c7
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-10-11 00:18:21 -0600

    Changed version # in `setup.py`

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(README_PATH, "r") as fh:
 
 setup(
     name = "spotifyCL",
-	version = "1.0.9",
+	version = "1.1.0",
     author = "Huck Dirksmeier",
     author_email = "Huckdirks@gmail.com",
     description = "A command line interface for Spotify",
