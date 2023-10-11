8295e93fd8d502d255385c47a64274ee535fc972
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-10-11 01:32:47 -0600

    All documentation added & version updated

--- spotifycl/spotify_status.py
+++ spotifycl/spotify_status.py
@@ -13,9 +13,8 @@ class SpotifyStatus:
         
     Methods
     -------
-    status(SONG_CHANGE: bool = False) -> bool
-        Get the current status of Spotify and print it.
-        Returns True if a song is currently playing, False otherwise
+    status(SONG_CHANGE: bool = False)
+        Get the current status of Spotify and print it
     
     convert_duration(PROGRESS: int, DURATION: int) -> str
         Convert the progress and duration from ms to minutes and seconds and return a string
@@ -65,7 +64,7 @@ class SpotifyStatus:
             return f"{PROGRESS_MIN}:{PROGRESS_SEC} / {DURATION_MIN}:0{DURATION_SEC}"
 
         
-    def status(self, SONG_CHANGE: bool = False) -> bool:
+    def status(self, SONG_CHANGE: bool = False):
         """Print the current status of Spotify
         
         Example:
@@ -74,6 +73,8 @@ class SpotifyStatus:
             Artist: Pink Floyd
             Album: The Dark Side of the Moon
             Position: 4:20 / 6:53
+            
+        Will exit with an error message if there's no current song playing
         
         Parameters
         ----------
@@ -82,14 +83,13 @@ class SpotifyStatus:
             
         Returns
         -------
-        bool
-            True if a song is currently playing, False otherwise
+        None
         """
         
         STATUS = self.sp.current_playback()
         if STATUS is None:
             print("No song is currently playing")
-            return False
+            exit(1)
         
         if SONG_CHANGE:
             cprint("Spotify is Now Playing:", "green", attrs=["bold"])
@@ -101,4 +101,4 @@ class SpotifyStatus:
         cprint("Album: " + str(STATUS["item"]["album"]["name"]), "cyan")
         if not SONG_CHANGE:
             cprint("Position: " + str(self.convert_duration(int(STATUS["progress_ms"] / 1000), int(STATUS["item"]["duration_ms"] / 1000))), "cyan")
-        return True
+        return
