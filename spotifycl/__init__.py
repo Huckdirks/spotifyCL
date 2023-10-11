e111ef89c0ae952d98107944ca2651e912e00b70
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-10-11 00:34:23 -0600

    Added documentation to `__init__.py`

---
+++ spotifycl/__init__.py
@@ -0,0 +1,15 @@
+""" Spotify Command Line
+
+This Program allows you to control Spotify from the command line.
+
+This program requires a Spotify Premium account, and these packages:
+    - python-dotenv
+    - spotipy
+    - termcolor
+
+To install, run:
+    pip install spotifycl
+    
+To use, run:
+    spotifycl --help
+"""
