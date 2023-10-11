fdbf9262a043fb2c220e845aec020382d07e1d8b
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-10-11 00:42:54 -0600

    Added documentation to `search.py`

--- spotifycl/search.py
+++ spotifycl/search.py
@@ -1,7 +1,17 @@
 # External Libraries
 import spotipy
 
-def search(sp: spotipy.Spotify, NAME: str, TYPE: str):
+def search(sp: spotipy.Spotify, NAME: str, TYPE: str) -> dict | None:
+    """Search Spotify for a song, album, artist, or playlist
+    
+    Returns
+    -------
+    dict
+        The first result from the search
+    
+    None
+        If no results are found
+    """
     RESULT = sp.search(NAME, type=TYPE, limit=1)
     if RESULT[TYPE + 's']['total'] == 0:
         return None
