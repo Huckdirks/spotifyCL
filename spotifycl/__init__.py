b5e808ddb8b9fca1ae3fb036984d07478f41a32a
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-10-10 19:48:34 -0600

    All errors for `spotify_play.py` done!

--- spotifycl/__init__.py
+++
@@ -1,7 +0,0 @@
-""" from .cli import *
-from .set_env import *
-from .spotify import *
-from .spotify_play import *
-from .spotify_queue import *
-from .spotify_status import *
-from .set_credentials import * """
--- spotifycl/spotify_play.py
+++ spotifycl/spotify_play.py
@@ -11,56 +11,96 @@ class SpotifyPlay:
 
     def play_pause(self):
         # If the song is playing, pause it
-        if self.sp.current_playback()['is_playing']:
+        """ if self.sp.current_playback()['is_playing']:
             self.sp.pause_playback()
         else:
             self.sp.start_playback()
-        return
+        return """
+        try:
+            if self.sp.current_playback()['is_playing']:
+                self.sp.pause_playback()
+            else:
+                self.sp.start_playback()
+        except:
+            print("No Current Spotify Session!")
+            exit(1)
     
     def play_track(self, NAME: str):
         RESULT = search(self.sp, NAME, 'track')
         if RESULT:
-            URI = RESULT['uri']
-            self.sp.start_playback(uris=[URI])
-            cprint("Spotify Playing:", "green", attrs=["bold"])
-            cprint("Song: " + str(RESULT["name"]), "cyan")
-            cprint("Artist: " + str(RESULT["artists"][0]["name"]), "cyan")
-            cprint("Album: " + str(RESULT["album"]["name"]), "cyan")
+            try:
+                URI = RESULT['uri']
+                self.sp.start_playback(uris=[URI])
+                cprint("Spotify Playing:", "green", attrs=["bold"])
+                cprint("Song: " + str(RESULT["name"]), "cyan")
+                cprint("Artist: " + str(RESULT["artists"][0]["name"]), "cyan")
+                cprint("Album: " + str(RESULT["album"]["name"]), "cyan")
+            except:
+                #print("No Current Spotify Session!") # Spotipy already sends an error message
+                exit(1)
+        else:
+            print("No song found")
+            exit(1)
         return
     
     def play_album(self, NAME: str):
         RESULT = search(self.sp, NAME, 'album')
         if RESULT:
-            URI = RESULT['uri']
-            self.sp.shuffle(state=False)
-            self.sp.start_playback(context_uri=URI)
-            cprint("Spotify Playing:", "green", attrs=["bold"])
-            cprint("Album: " + str(RESULT["name"]), "cyan")
-            cprint("Artist: " + str(RESULT["artists"][0]["name"]), "cyan")
+            try:
+                URI = RESULT['uri']
+                self.sp.shuffle(state=False)
+                self.sp.start_playback(context_uri=URI)
+                cprint("Spotify Playing:", "green", attrs=["bold"])
+                cprint("Album: " + str(RESULT["name"]), "cyan")
+                cprint("Artist: " + str(RESULT["artists"][0]["name"]), "cyan")
+            except:
+                #print("No Current Spotify Session!") # Spotipy already sends an error message
+                exit(1)
+        else:
+            print("No album found")
+            exit(1)
         return
     
     def play_artist(self, NAME: str):
         RESULT = search(self.sp, NAME, 'artist')
         if RESULT:
-            URI = RESULT['uri']
-            self.sp.start_playback(context_uri=URI)
-            cprint("Spotify Playing:", "green", attrs=["bold"])
-            cprint("Artist: " + str(RESULT["name"]), "cyan")
+            try:
+                URI = RESULT['uri']
+                self.sp.start_playback(context_uri=URI)
+                cprint("Spotify Playing:", "green", attrs=["bold"])
+                cprint("Artist: " + str(RESULT["name"]), "cyan")
+            except:
+                #print("No Current Spotify Session!") # Spotipy already sends an error message
+                exit(1)
+        else:
+            print("No artist found")
+            exit(1)
         return
     
     # Make this go through your playlists first, then if it can't find it, search everything
     def play_playlist(self, NAME: str):
         RESULT = search(self.sp, NAME, 'playlist')
         if RESULT:
-            URI = RESULT['uri']
-            self.sp.start_playback(context_uri=URI)
-            cprint("Spotify Playing:", "green", attrs=["bold"])
-            cprint("Playlist: " + str(RESULT["name"]), "cyan")
-            cprint("Owner: " + str(RESULT["owner"]["display_name"]), "cyan")
+            try:
+                URI = RESULT['uri']
+                self.sp.start_playback(context_uri=URI)
+                cprint("Spotify Playing:", "green", attrs=["bold"])
+                cprint("Playlist: " + str(RESULT["name"]), "cyan")
+                cprint("Owner: " + str(RESULT["owner"]["display_name"]), "cyan")
+            except:
+                #print("No Current Spotify Session!") # Spotipy already sends an error message
+                exit(1)
+        else:
+            print("No playlist found")
+            exit(1)
         return
     
     def play_uri(self, URI: str):
-        self.sp.start_playback(uris=[URI])
-        cprint("Spotify Playing:", "green", attrs=["bold"])
-        cprint("URI: " + str(URI), "cyan")
+        try:
+            self.sp.start_playback(uris=[URI])
+            cprint("Spotify Playing:", "green", attrs=["bold"])
+            cprint("URI: " + str(URI), "cyan")
+        except:
+            #print("No Current Spotify Session!") # Spotipy already sends an error message
+            exit(1)
         return
