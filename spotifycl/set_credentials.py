14cb29471a1277e9d6400163f2d1b70c11979d49
hdirks2 <114555560+hdirks2@users.noreply.github.com>
2023-10-11 00:45:14 -0600

    Added documentation to `set_credentials.py`

--- spotifycl/set_credentials.py
+++ spotifycl/set_credentials.py
@@ -8,10 +8,18 @@ from dotenv import load_dotenv
 
 
 def set_credentials() -> spotipy.Spotify:
+    """Set the credentials for the Spotify API
+    
+    Returns
+    -------
+    spotipy.Spotify
+        The Spotify API object
+    """
+    
     ENV_PATH: str = join(dirname(__file__), '.env')
     if not isfile(ENV_PATH):
         print(".env file not found")
-        exit()
+        exit(1)
     
     load_dotenv(ENV_PATH)
     SCOPE = "user-read-playback-state user-modify-playback-state"
