cdd5218a9840d617c1ee91fc07074dafcc49a24d
Huck Dirksmeier <114555560+Huckdirks@users.noreply.github.com>
2023-10-12 23:40:19 -0700

    Possibly fixed the .cache problem

--- spotifycl/set_credentials.py
+++ spotifycl/set_credentials.py
@@ -16,12 +16,19 @@ def set_credentials() -> spotipy.Spotify:
         The Spotify API object
     """
     
-    ENV_PATH: str = join(dirname(__file__), '.env')
+    #ENV_PATH: str = join(dirname(__file__), '.env')
+    PROJECT_DIR: str = dirname(__file__)
+    
+    ENV_PATH: str = f"{PROJECT_DIR}.env"
     if not isfile(ENV_PATH):
         print(".env file not found")
         exit(1)
     
     load_dotenv(ENV_PATH)
     SCOPE = "user-read-playback-state user-modify-playback-state"
-    sp: spotipy.Spotify = spotipy.Spotify(client_credentials_manager = SpotifyOAuth(scope = SCOPE))
+    CACHE_PATH: str = f"{PROJECT_DIR}.cache"
+    cache_handle = CacheFileHandler(cache_path = CACHE_PATH)
+    credentials_manager = SpotifyOAuth(scope = SCOPE, cache_handler = cache_handle)
+    sp = spotify.Spotify(client_credentials_manager = credentials_manager)
+    #sp: spotipy.Spotify = spotipy.Spotify(client_credentials_manager = SpotifyOAuth(scope = SCOPE))
     return sp
